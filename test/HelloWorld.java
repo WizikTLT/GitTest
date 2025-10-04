00b567f594063caf1f06efe4711f8be11c19815d
Wiz173 <Wiz173@mail.ru>
2025-10-04 12:32:31 +0400

    squishED

--- test/HelloWorld.java
+++ test/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World 2");
+        System.out.println("Hello before Squish");
+        System.out.println("Hello after Squish");
     }
 }
