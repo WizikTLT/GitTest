8b3b9ed4bb255302b26af950663c0e9ab9e876e1
Wiz173 <Wiz173@mail.ru>
2025-10-04 12:38:57 +0400

    review_2

--- test/HelloWorld.java
+++ test/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World 2");
+        System.out.println("FIXED BY REWIEV");
+        System.out.println("FIXED BY REWIEV2");
         System.out.println("Hello before Squish");
         System.out.println("Hello after Squish");
     }
