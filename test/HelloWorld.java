7a9c23d8dd1be7beb78c91cfb774334aaae6f90c
Wiz173 <Wiz173@mail.ru>
2025-10-06 16:25:11 +0400

    Пиу

--- test/HelloWorld.java
+++ test/HelloWorld.java
@@ -3,6 +3,6 @@ import javax.xml.transform.Source;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Идеально работающая программа_____");
-
+        System.out.println("Привет от разраба_2");
     }
 }
