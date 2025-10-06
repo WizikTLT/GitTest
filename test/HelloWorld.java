8ef25b8e6e142dd070d7c0ed2f339b537e358cdf
Wiz173 <Wiz173@mail.ru>
2025-10-06 16:00:10 +0400

    Пиу

--- test/HelloWorld.java
+++ test/HelloWorld.java
@@ -2,14 +2,6 @@ import javax.xml.transform.Source;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World 2");
-        System.out.println("FIXED BY REVIEW");
-        System.out.println("FIXED BY REVIEW2");
-        System.out.println("FIXED BY REVIEW3");
-        System.out.println("FIXED BY REVIEW4");
-        System.out.println("FIXED BY REVIEW5");
-        System.out.println("Hello before Squish");
-        System.out.println("Hello after Squish");
+        System.out.println("Идеально работающая программа");
     }
 }
